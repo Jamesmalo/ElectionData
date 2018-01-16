b2a691a146202799b2a39ef8c2ae62260ad5091d
Jamesmalo <9jmalo@stpatrick.org>
2018-01-16 14:44:13 -0600

    day 1

---
+++ electionClient.js
@@ -0,0 +1,4 @@
+const FileReader = require('./FileReader.js');
+const electionParser = require('./electionParser.js');
+
+let list = FR.LoadFileIntoArrayByLine('electionSimplified.csv');
---
+++ electionParser.js
@@ -0,0 +1,14 @@
+function electionData(array){
+  this.statePostal = array[0];
+  this.county = array[1];
+  this.fips = array[2];
+  this.obamaVote = array[3];
+  this.obamaPercentage = array[4];
+  this.romneyVote = array[5];
+  this.romneyPercentage = array[6];
+
+}
+
+function commaParser(){
+
+}
